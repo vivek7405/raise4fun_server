1a977b723d0e1992b257d00cfedce508e8807b37
Vivek <vivek7405@gmail.com>
2021-04-23 23:07:21 +0530

    Initial commit

---
+++ src/routes/users.route.ts
@@ -0,0 +1,26 @@
+import { Router } from 'express';
+import UsersController from '../controllers/users.controller';
+import { CreateUserDto } from '../dtos/users.dto';
+import Route from '../interfaces/routes.interface';
+import authMiddleware from '../middlewares/auth.middleware';
+import validationMiddleware from '../middlewares/validation.middleware';
+
+class UsersRoute implements Route {
+  public path = '/users';
+  public router = Router();
+  public usersController = new UsersController();
+
+  constructor() {
+    this.initializeRoutes();
+  }
+
+  private initializeRoutes() {
+    this.router.get(`${this.path}`, authMiddleware, this.usersController.getUsers);
+    this.router.get(`${this.path}/:id`, authMiddleware, this.usersController.getUserById);
+    this.router.post(`${this.path}`, validationMiddleware(CreateUserDto, 'body'), this.usersController.createUser);
+    this.router.put(`${this.path}/:id`, authMiddleware, validationMiddleware(CreateUserDto, 'body', true), this.usersController.updateUser);
+    this.router.delete(`${this.path}/:id`, authMiddleware, this.usersController.deleteUser);
+  }
+}
+
+export default UsersRoute;
